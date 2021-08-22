e26a6b84a90bc0438829cfbaddb025e455f9c0ad
wxnacy <wxnacy@gmail.com>
2021-08-22 12:40:25 +0800

    U: 增加 lfsdu 命令

--- lfsdb/cli/lfs.py
+++ lfsdb/cli/lfs.py
@@ -17,14 +17,18 @@ def main():
     _db = args[0]
     table = args[1]
     _id = args[2]
+    fields = []
+    if len(args) > 3:
+        fields = args[3:]
     item = db.get_db(_db).get_table(table).find_one_by_id(_id)
-    print(json.dumps(item, indent=4))
-    #  print('s')
-    #  task = db.get_db('download').get_table('task')
-    #  #  task.insert({"name": "test"})
-    #  items = task.find()
-    #  for item in items:
-        #  print(json.dumps(item, indent=4))
+    data = {}
+    print(fields)
+    if fields:
+        for f in fields:
+            data[f] = item.get(f)
+    else:
+        data = item
+    print(json.dumps(data, indent=4, ensure_ascii=False))
 
 if __name__ == "__main__":
     main()
