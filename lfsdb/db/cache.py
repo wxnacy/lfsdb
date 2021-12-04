c3a65881f9a1b6eadf1de403ea8c7baebb0d1277
wxnacy <wxnacy@gmail.com>
2021-12-04 23:47:02 +0800

    U: 增加 CacheTable 类

--- lfsdb/db/cache.py
+++ lfsdb/db/cache.py
@@ -2,60 +2,51 @@
 # -*- coding:utf-8 -*-
 # Author: wxnacy@gmail.com
 """
-数据缓存
+缓存存储
 """
 
-import os
-import json
-import sys
+from lfsdb.db.base import BaseTable
+from lfsdb.common.cache import CACHE
 
-from collections import defaultdict
 
-class FileCache(object):
-    _table_data = defaultdict(dict)
+class CacheTable(BaseTable):
 
-    def init(self, db, table):
-        self.db = db
-        self.table = table
+    cache = CACHE
 
-    def _generage_key(self):
-        """生成 key"""
-        return '{}_{}'.format(self.db, self.table)
-
-    def set(self, data):
-        """插入数据"""
-        key = self._generage_key()
-        if isinstance(data, dict) and '_id' in data:
-            self._table_data[key][data['_id']] = data
-        #  print(json.dumps(self._table_data, indent=4))
-
-    def get(self, _id):
-        """通过 id 查找"""
-        return None
-        key = self._generage_key()
-        return self._table_data[key].get(_id)
-
-    def remove(self, _id):
-        """通过 id 查找"""
-        key = self._generage_key()
-        return self._table_data[key].pop(_id, None)
-
-    def list_ids(self,):
-        """列出 id 列表"""
-        key = self._generage_key()
-        data = self._table_data.get(key)
-
-        return []
-        return list(data.keys()) if data else []
+    def __init__(self, db, table):
+        super().__init__(db, table)
+        self.key = '{}-{}'.format(db, table)
 
     def drop(self):
         """删除表"""
-        self._table_data[self._generage_key()] = {}
+        self.cache.hdrop(self.key)
 
-    def memory(self):
-        """内存"""
-        return sys.getsizeof(self._table_data)
+    def _delete_by_id(self, _id):
+        """根据 _id 删除
+        如果成功需要返回 True
+        """
+        return self.cache.hdel(self.key, _id) == 1
 
-FILE_CACHE = FileCache()
+    def _update(self, doc):
+        """修改数据
+        如果成功需要返回 True
+        """
+        _id = doc.get("_id")
+        self.cache.hset(self.key, _id, doc)
+        return True
 
-del FileCache
+    def _list_ids(self):
+        """列出当前表内的 id 列表"""
+        return self.cache.hkeys(self.key)
+
+    def _read_by_id(self, _id):
+        """使用 _id 读取数据"""
+        return self.cache.hget(self.key, _id)
+
+    def _exists_id(self, _id):
+        """是否存在 _id"""
+        return bool(self.cache.hget(self.key, _id))
+
+    def _write(self, doc):
+        """写入数据"""
+        self.cache.hset(self.key, doc['_id'], doc)
